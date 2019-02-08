65f7cefb98ae998cd8eed5cb429457c9f179aef2
jdaintry <47262749+jdaintry@users.noreply.github.com>
2019-02-09 10:52:26 +1100

    Completed the module

--- WishList/Models/AccountViewModels/LoginViewModel.cs
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WishList.Models.AccountViewModels
 {
     public class LoginViewModel
     {
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
     }
 }
