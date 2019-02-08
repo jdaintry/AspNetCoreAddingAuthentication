4f8b1237a7140a851f63b9c9fa8580e587d64bbf
jdaintry <47262749+jdaintry@users.noreply.github.com>
2019-02-09 10:24:27 +1100

    Completed the module

--- WishList/Models/AccountViewModels/LoginViewModel.cs
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -9,10 +9,7 @@ namespace WishList.Models.AccountViewModels
     public class LoginViewModel
     {
 
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-
+       
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
